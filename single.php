af6de29c0fa2303d7c6802776c64a46051968663
Christian <cunigarrod@gmail.com>
2021-01-31 15:16:59 -0500

    Post pages styles added.

---
+++ single.php
@@ -0,0 +1,89 @@
+<?php get_header(); ?>
+
+<img src="https://via.placeholder.com/1500x400" alt="">
+
+<div class="container mx-auto px-4 py-10">
+  <div class="grid gap-10 grid-cols-12">
+    <div class="col-span-1">
+      <ul>
+        <li class="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center mb-3">
+          <i class="mdi mdi-twitter mdi-24px"></i>
+        </li>
+        <li class="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center mb-3">
+          <i class="mdi mdi-facebook mdi-24px"></i>
+        </li>
+        <li class="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center mb-3">
+          <i class="mdi mdi-whatsapp mdi-24px"></i>
+        </li>
+        <li class="w-10 h-10 bg-gray-100 rounded-full flex justify-center items-center mb-3">
+          <i class="mdi mdi-email-outline mdi-24px"></i>
+        </li>
+      </ul>
+    </div>
+    <div class="col-span-8 pt-5">
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </h2>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio magnam corporis, assumenda veniam sapiente omnis qui repudiandae asperiores totam itaque commodi! Quod, nisi ad officia sapiente atque similique totam eius?
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta a sit minima modi, architecto quidem sunt illo numquam praesentium? Laboriosam beatae consequuntur doloremque repellat error molestias alias inventore maxime libero.
+      </p>
+
+      <hr class="border-yellow-600 border-1 my-10">
+      <div class="flex">
+        <div class="w-24 h-24 bg-gray-200 rounded-full mr-5"></div>
+        <div class="flex-1">
+          <div class="mb-4 flex items-center">
+            <p class="font-bold flex-1">
+              Maritza Gómez
+            </p>
+            <ul class="flex">
+              <li class="w-8 h-8 bg-gray-100 rounded-full flex justify-center items-center mr-2">
+                <i class="mdi mdi-twitter mdi-18px"></i>
+              </li>
+              <li class="w-8 h-8 bg-gray-100 rounded-full flex justify-center items-center">
+                <i class="mdi mdi-email-outline mdi-18px"></i>
+              </li>
+            </ul>
+          </div>
+          <p class="mb-3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, molestias iure sit inventore reprehenderit voluptates et alias voluptas eaque in maxime neque odio, dolor dolorem enim ratione nostrum sunt. Corporis.
+          </p>
+          <p class="text-right text-sm font-bold">
+            Leer más
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-span-3">
+      <hr class="border-yellow-600 w-5/12 border-1 mb-4">
+      <h3 class="mb-4">
+        Lo más leído
+      </h3>
+      <div class="mb-6">
+        <img class="mb-3" src="https://via.placeholder.com/400x200" alt="">
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div class="mb-6">
+        <img class="mb-3" src="https://via.placeholder.com/400x200" alt="">
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div class="mb-6">
+        <img class="mb-3" src="https://via.placeholder.com/400x200" alt="">
+        <p class="text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
